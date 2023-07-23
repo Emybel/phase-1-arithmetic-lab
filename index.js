74ced4e9e732ddb8f68e6037c1341b6468c1615e
Imane Belaid <belaidimane@gmail.com>
2023-07-23 11:41:11 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// Multiply equation
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+// Random Int generator
+let random = Number.isInteger(random) + 1
+
+// Mod equation
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+// Max method
+let max = Math.max(2,7,13,18,20,0);
+
